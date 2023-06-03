556af7ac9cad8d2e9ba49d800b8e37faf1618bc2
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-06-03 19:32:52 +0530

    Navbar and Hero section designed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <nav className="bg-gray-700">
+        <nav className="bg-[#6F6FDB]">
             <div className="container mx-auto px-4">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
@@ -19,7 +19,7 @@ const Navbar = () => {
                         <Link href="/contact" className="rounded-md text-gray-300 px-3 py-2 hover:text-white hover:bg-gray-700">
                             Contact
                         </Link>
-                        
+
                     </div>
                     <div className="md:hidden">
                         {/* Add your responsive menu button here */}
--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,15 @@ type Props = {}
 const Home: React.FC = (props: Props) => {
 
   return (
-    <div className="dark:bg-slate-800 dark:text-slate-50 text-gray-900 bg-gray-100 min-h-screen">
+    <div className="dark:bg-slate-800  dark:text-slate-50 text-gray-900 bg-gray-100 min-h-screen">
 
-      <div className="container mx-auto px-4 py-12">
-        <h1 className="text-4xl font-bold ">Welcome to Brogrammer Blog</h1>
-        <p className="dark:text-slate-100 text-gray-600 mt-4">Discover amazing articles and insights.</p>
+      <div className=" flex justify-between home items-center md:px-32 px-5 py-12">
+        <div>
+          <h1 className="text-4xl text-[#2D2D36] font-bold">
+            <span className='hidden md:inline'>Welcome to </span>Brogrammer Blog</h1>
+          <p className="dark:text-slate-100 text-gray-700 mt-4">Discover amazing articles and insights.</p>
+        </div>
+        <img src="coding.png" className="hidden md:flex" width={350} />
       </div>
       <section className='m-5'>
         <h2 className='text-3xl font-bold'>Latest Blogs</h2>
