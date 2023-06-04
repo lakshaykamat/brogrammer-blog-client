df2a8893045e239f39406691d18c012272d35218
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-06-05 00:03:29 +0530

    Minor changes in css layout of home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-100`}>
+      <body className={`${inter.className} bg-slate-100 dark:bg-blue-950`}>
         <Navbar/>
         {children}
         <Footer/>
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,176 @@
 import React from 'react'
 import AllBLogs from './components/BlogCard';
+import Link from 'next/link';
 
 type Props = {}
 
 const Home: React.FC = (props: Props) => {
 
   return (
-    <div className=" ">
+    <div className="">
 
-      <div className="drop-shadow-2xl home_gradient flex justify-between items-center md:px-32 px-5 py-12">
-        <div>
-          <h1 className="text-4xl text-[#2D2D36] font-bold">
-            <span className='hidden  md:inline'>Welcome to </span>Brogrammer Blog</h1>
-          <p className="text-gray-700 mt-4">Discover amazing articles and insights.</p>
+      <div className='m-12 xl:mx-48'>
+        <div className='flex gap-3 flex-col my-3'>
+          <h1 className='text-3xl xl:text-4xl font-bold'>Brogramer Blog</h1>
+          <p className='text-lg xl:text-xl md:w-[80%]'>Get tips and advice on delivering exceptional customer service, engaging and delighting your customers, and building a customer-centric company.</p>
         </div>
-        <img src="coding.png" className="hidden md:flex" width={350} />
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 flex-wrap justify-around items-center my-12'>
+          <button className='text-slate-100 px-16 py-4 text-base lg:text-lg drop-shadow-lg font-bold bg-sky-500 rounded tracking-wider hover:bg-sky-600 transition-all'>Reactjs</button>
+          <button className='text-slate-100 px-16 py-4 text-base lg:text-lg drop-shadow-lg font-bold bg-sky-500 rounded tracking-wider hover:bg-sky-600 transition-all'>C++</button>
+          <button className='text-slate-100 px-16 py-4 text-base lg:text-lg drop-shadow-lg font-bold bg-sky-500 rounded tracking-wider hover:bg-sky-600 transition-all'>Python</button>
+          <button className='text-slate-100 px-16 py-4 text-base lg:text-lg drop-shadow-lg font-bold bg-sky-500 rounded tracking-wider hover:bg-sky-600 transition-all'>Kotlin</button>
+        </div>
       </div>
+      <div className="m-12 xl:mx-48">
+        <h1 className="text-3xl font-bold">Most Recent Posts</h1>
+        <hr className="h-1 my-6 bg-gray-600" />
+
+        {/* Most recent post */}
+        {/* lg:visible hidden One Big Card in left and other post in right vertically */}
+        <div className="hidden lg:visible lg:flex justify-between gap-12">
+
+          <div className="items-center justify-stretch rounded-lg">
+            <div className="relative overflow-hidden">
+              <img
+                src="https://public-images.interaction-design.org/literature/articles/heros/article_130515_hero_636e39613117f1.68703759.jpg"
+                alt="Best Practices for Optimizing Website Performance and Loading Speed"
+                className="transition-all hover:scale-110 w-full h-auto object-cover mb-4 rounded-lg"
+              />
+            </div>
+            <div className="mt-auto text-gray-800 text-sm flex gap-3">
+              <p>5 Jan 2023</p>
+              <p>Lakshay</p>
+            </div>
+            <div className="flex flex-col">
+              <Link
+                href="/posts/best-practices-for-optimizing-web-performance-and-loading-speed"
+                className="text-lg xl:text-2xl text-gray-800 mb-4 mt-2 font-bold"
+              >
+                Best Practices for Optimizing Website Performance and Loading Speed
+              </Link>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-6 w-1/3">
+
+            <div className="items-center justify-stretch rounded-lg">
+              <div className="relative overflow-hidden">
+                <img
+                  src="https://public-images.interaction-design.org/literature/articles/heros/article_130515_hero_636e39613117f1.68703759.jpg"
+                  alt="Best Practices for Optimizing Website Performance and Loading Speed"
+                  className="transition-all hover:scale-110 w-full h-auto object-cover mb-4 rounded-lg"
+                />
+              </div>
+              <div className="mt-auto text-gray-800 text-sm flex gap-3">
+                <p>5 Jan 2023</p>
+                <p>Lakshay</p>
+              </div>
+              <div className="flex flex-col">
+                <Link
+                  href="/posts/best-practices-for-optimizing-web-performance-and-loading-speed"
+                  className="text-lg xl:text-2xl text-gray-800 mb-4 mt-2 font-bold"
+                >
+                  Best Practices for Optimizing Website Performance and Loading Speed
+                </Link>
+              </div>
+            </div>
+
+            <div className="items-center justify-stretch rounded-lg">
+              <div className="relative overflow-hidden">
+                <img
+                  src="https://public-images.interaction-design.org/literature/articles/heros/article_130515_hero_636e39613117f1.68703759.jpg"
+                  alt="Best Practices for Optimizing Website Performance and Loading Speed"
+                  className="transition-all hover:scale-110 w-full h-auto object-cover mb-4 rounded-lg"
+                />
+              </div>
+              <div className="mt-auto text-gray-800 text-sm flex gap-3">
+                <p>5 Jan 2023</p>
+                <p>Lakshay</p>
+              </div>
+              <div className="flex flex-col">
+                <Link
+                  href="/posts/best-practices-for-optimizing-web-performance-and-loading-speed"
+                  className="text-lg xl:text-2xl text-gray-800 mb-4 mt-2 font-bold"
+                >
+                  Best Practices for Optimizing Website Performance and Loading Speed
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Visible at smaller screens */}
+        <div className="lg:hidden">
+          <div>
+            <div className="relative overflow-hidden">
+              <img
+                src="https://public-images.interaction-design.org/literature/articles/heros/article_130515_hero_636e39613117f1.68703759.jpg"
+                alt="Best Practices for Optimizing Website Performance and Loading Speed"
+                className="transition-all hover:scale-110 w-full h-auto object-cover mb-4 rounded-lg"
+              />
+            </div>
+            <div className="mt-auto text-gray-800 text-sm flex gap-3">
+              <p>5 Jan 2023</p>
+              <p>Lakshay</p>
+            </div>
+            <div className="flex flex-col">
+              <Link
+                href="/posts/best-practices-for-optimizing-web-performance-and-loading-speed"
+                className="text-lg xl:text-2xl text-gray-800 mb-4 mt-2 font-bold"
+              >
+                Best Practices for Optimizing Website Performance and Loading Speed
+              </Link>
+            </div>
+          </div>
+
+          <div className="flex gap-6 flex-col md:flex-row">
+            <div>
+              <div className="relative overflow-hidden">
+                <img
+                  src="https://public-images.interaction-design.org/literature/articles/heros/article_130515_hero_636e39613117f1.68703759.jpg"
+                  alt="Best Practices for Optimizing Website Performance and Loading Speed"
+                  className="transition-all hover:scale-110 w-full h-auto object-cover mb-4 rounded-lg"
+                />
+              </div>
+              <div className="mt-auto text-gray-800 text-sm flex gap-3">
+                <p>5 Jan 2023</p>
+                <p>Lakshay</p>
+              </div>
+              <div className="flex flex-col">
+                <Link
+                  href="/posts/best-practices-for-optimizing-web-performance-and-loading-speed"
+                  className="text-lg xl:text-2xl text-gray-800 mb-4 mt-2 font-bold"
+                >
+                  Best Practices for Optimizing Website Performance and Loading Speed
+                </Link>
+              </div>
+            </div>
+
+            <div>
+              <div className="relative overflow-hidden">
+                <img
+                  src="https://public-images.interaction-design.org/literature/articles/heros/article_130515_hero_636e39613117f1.68703759.jpg"
+                  alt="Best Practices for Optimizing Website Performance and Loading Speed"
+                  className="transition-all hover:scale-110 w-full h-auto object-cover mb-4 rounded-lg"
+                />
+              </div>
+              <div className="mt-auto text-gray-800 text-sm flex gap-3">
+                <p>5 Jan 2023</p>
+                <p>Lakshay</p>
+              </div>
+              <div className="flex flex-col">
+                <Link
+                  href="/posts/best-practices-for-optimizing-web-performance-and-loading-speed"
+                  className="text-lg xl:text-2xl text-gray-800 mb-4 mt-2 font-bold"
+                >
+                  Best Practices for Optimizing Website Performance and Loading Speed
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
 
       {/* Latest Blog  */}
       <section className="mx-10 md:mx-12 lg:mx-32 xl:mx-40 my-14">
@@ -33,7 +188,7 @@ const Home: React.FC = (props: Props) => {
           <AllBLogs />
         </div>
       </section>
-    </div>
+    </div >
   )
 }
 export default Home
