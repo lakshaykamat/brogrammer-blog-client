683020ffc7024f59e148ce85c30869bbc6b5ed85
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-06-09 00:38:23 +0530

    configured all domain images

--- next.config.js
+++ next.config.js
@@ -2,12 +2,11 @@
 
 module.exports = {
     images: {
-        domains: [
-            'miro.medium.com',
-            'public-images.interaction-design.org',
-            'assets.website-files.com',
-            'www.educative.io',
-            'res.cloudinary.com'
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "**",
+          },
         ],
       },
 }
