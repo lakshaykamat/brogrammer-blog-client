43ecea183248b34552e68eac92077da03c146e90
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-06-09 00:40:13 +0530

    Modified BlogPost Type

--- types.d.ts
+++ types.d.ts
@@ -1,145 +1,62 @@
 export type BlogPost = {
-    data: Array<{
-      id: number
-      attributes: {
-        title: string
-        createdAt: string
-        updatedAt: string
-        publishedAt: string
-        content: string
-        description?: string
-        slug: string
-        author: string
-        creation: string
-        category: string
-        locale: string
-        coverImageURL: string
-        coverImage: {
-          data: {
-            id: number
-            attributes: {
-              name: string
-              alternativeText: any
-              caption: any
-              width: number
-              height: number
-              formats: {
-                small?: {
-                  ext: string
-                  url: string
-                  hash: string
-                  mime: string
-                  name: string
-                  path: any
-                  size: number
-                  width: number
-                  height: number
-                }
-                medium?: {
-                  ext: string
-                  url: string
-                  hash: string
-                  mime: string
-                  name: string
-                  path: any
-                  size: number
-                  width: number
-                  height: number
-                }
-                thumbnail: {
-                  ext: string
-                  url: string
-                  hash: string
-                  mime: string
-                  name: string
-                  path: any
-                  size: number
-                  width: number
-                  height: number
-                }
-                large?: {
-                  ext: string
-                  url: string
-                  hash: string
-                  mime: string
-                  name: string
-                  path: any
-                  size: number
-                  width: number
-                  height: number
-                }
-              }
-              hash: string
-              ext: string
-              mime: string
-              size: number
-              url: string
-              previewUrl: any
-              provider: string
-              provider_metadata: any
-              createdAt: string
-              updatedAt: string
-            }
+  data: Array<{
+    id: number
+    attributes: {
+      title: string
+      createdAt: string
+      updatedAt: string
+      publishedAt: string
+      content: string
+      description?: string
+      slug: string
+      author: string
+      creation: string
+      locale: string
+      coverImageURL: string
+      authorImageURL: string
+      categoryF: string
+      category: {
+        data: {
+          id: number
+          attributes: {
+            createdAt: string
+            updatedAt: string
+            publishedAt: string
+            name: string
           }
-        }
-        authorImage: {
-          data: {
-            id: number
-            attributes: {
-              name: string
-              alternativeText: string
-              caption: string
-              width: number
-              height: number
-              formats: {
-                small: {
-                  ext: string
-                  url: string
-                  hash: string
-                  mime: string
-                  name: string
-                  path: any
-                  size: number
-                  width: number
-                  height: number
-                }
-                thumbnail: {
-                  ext: string
-                  url: string
-                  hash: string
-                  mime: string
-                  name: string
-                  path: any
-                  size: number
-                  width: number
-                  height: number
-                }
-              }
-              hash: string
-              ext: string
-              mime: string
-              size: number
-              url: string
-              previewUrl: any
-              provider: string
-              provider_metadata: any
-              createdAt: string
-              updatedAt: string
-            }
-          }
-        }
-        localizations: {
-          data: Array<any>
         }
       }
-    }>
-    meta: {
-      pagination: {
-        page: number
-        pageSize: number
-        pageCount: number
-        total: number
+      localizations: {
+        data: Array<any>
       }
     }
+  }>
+  meta: {
+    pagination: {
+      page: number
+      pageSize: number
+      pageCount: number
+      total: number
+    }
   }
-  
+}
+
+export type Categories = {
+  data: Array<{
+    id: number
+    attributes: {
+      createdAt: string
+      updatedAt: string
+      publishedAt: string
+      name: string
+    }
+  }>
+  meta: {
+    pagination: {
+      page: number
+      pageSize: number
+      pageCount: number
+      total: number
+    }
+  }
+}
