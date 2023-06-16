8fbdfcb7473e28c45eb11a6edc4b762b76bb5904
Ruchi <ruchisingh17062005@gmail.com>
2023-06-16 12:26:13 +0530

    Maybe this works

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -6,6 +6,7 @@ const DashBoard = (props: Props) => {
   return (
     
 <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <h1 className='tex-3xl font-bold my-12'>Testing chages</h1>
     <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
             <tr>
